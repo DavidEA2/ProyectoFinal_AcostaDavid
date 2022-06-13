5d5915bc46189eb16263d57285942dfd07b5b880
DavidEA2 <94660486+DavidEA2@users.noreply.github.com>
2022-06-13 20:49:15 -0300

    Componentes login y home
    Creación de los componentes Login y Home.

--- MiPortfolio_AcostaDavid/Frontend/src/app/app.module.ts
+++ MiPortfolio_AcostaDavid/Frontend/src/app/app.module.ts
@@ -15,6 +15,8 @@ import { HardAndSoftSkillsComponent } from './components/hard-and-soft-skills/ha
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 @NgModule({
@@ -28,7 +30,9 @@ import { HttpClientModule } from '@angular/common/http';
     EducacionComponent,
     HardAndSoftSkillsComponent,
     ProyectosComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
